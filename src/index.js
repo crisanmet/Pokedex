bb19f4c8ad43da709fe66877fae2584da6cd8eb1
Cristian Sancricca <cristiansancricca@gmail.com>
2021-11-28 15:56:51 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const $links = document.querySelector(".links");
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+
+const obtenerPokemons = async () => {
+  try {
+    fetch();
+  } catch (error) {}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  obtenerPokemons();
+});
