6a0d91c7dd073db550abd9f58e60d63ea069314e
Cristian Sancricca <cristiansancricca@gmail.com>
2021-11-29 17:57:31 -0300

    se implementan algunas funciones y se suben img svg para loader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 const $links = document.querySelector(".links");
 const URL = "https://pokeapi.co/api/v2/pokemon/";
+const $main = document.querySelector(".main");
 
-const obtenerPokemons = async () => {
+const obtenerListadoPokemons = async () => {
   try {
-    fetch();
-  } catch (error) {}
+    let respuestaUrl = await fetch(URL);
+    let jsonRespuesta = await respuestaUrl.json();
+
+    console.log(jsonRespuesta);
+
+    obtenerUrlPokemons(jsonRespuesta);
+    if (!respuestaUrl.ok)
+      throw {
+        status: respuestaUrl.status,
+      };
+  } catch (err) {
+    console.log(err);
+    $main.innerHTML = `<p>Error: ${err.status}</p>`;
+  }
+};
+
+const obtenerUrlPokemons = async (pokemons) => {
+  for (let i = 0; i < pokemons.results.length; i++) {
+    //console.log(pokemon.results[i]);
+    try {
+      let respuestaPokemon = await fetch(pokemons.results[i].url);
+      let pokemon = await respuestaPokemon.json();
+      if (!respuestaPokemon.ok)
+        throw {
+          status: respuestaPokemon.status,
+        };
+      console.log(pokemon);
+    } catch (err) {
+      console.log(err);
+      $main.innerHTML = `<p>Error: ${err.status}</p>`;
+    }
+  }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  obtenerPokemons();
+  obtenerListadoPokemons();
 });
