5e7329a3a825ed4ab3ce63c660cae416a692b77b
Cristian Sancricca <cristiansancricca@gmail.com>
2021-11-30 17:56:08 -0300

    se implementa card de tailwind y en js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const $links = document.querySelector(".links");
 const URL = "https://pokeapi.co/api/v2/pokemon/";
 const $main = document.querySelector(".main");
+const $template = document.querySelector(".template-pokemon").content;
+const $fragment = document.createDocumentFragment();
 
 const obtenerListadoPokemons = async () => {
   try {
     let respuestaUrl = await fetch(URL);
     let jsonRespuesta = await respuestaUrl.json();
 
-    console.log(jsonRespuesta);
+    $main.innerHTML = `<div class="animate__animated animate__pulse animate__infinite">
+                          <img src="src/svg/pikachu.svg" alt="pikachu" class="block m-auto h-40"/>
+                       </div>`;
 
     obtenerUrlPokemons(jsonRespuesta);
     if (!respuestaUrl.ok)
@@ -22,20 +26,38 @@ const obtenerListadoPokemons = async () => {
 
 const obtenerUrlPokemons = async (pokemons) => {
   for (let i = 0; i < pokemons.results.length; i++) {
-    //console.log(pokemon.results[i]);
     try {
       let respuestaPokemon = await fetch(pokemons.results[i].url);
       let pokemon = await respuestaPokemon.json();
+
       if (!respuestaPokemon.ok)
         throw {
           status: respuestaPokemon.status,
         };
-      console.log(pokemon);
+      renderizarCardPokemon(pokemon);
     } catch (err) {
       console.log(err);
       $main.innerHTML = `<p>Error: ${err.status}</p>`;
     }
   }
+  $main.innerHTML = ``;
+  $main.appendChild($fragment);
+};
+
+const renderizarCardPokemon = (pokemon) => {
+  $template
+    .querySelector(".img-pokemon")
+    .setAttribute("src", pokemon.sprites.other.dream_world.front_default);
+
+  $template.querySelector(".nombre").textContent = pokemon.name;
+  $template.querySelector(".tipo1").textContent = pokemon.types[0].type.name;
+  $template.querySelector("#color-tipo").className = pokemon.types[0].type.name;
+  $template.querySelector(".ataque").textContent = pokemon.stats[1].base_stat;
+  $template.querySelector(".defensa").textContent = pokemon.stats[2].base_stat;
+  $template.querySelector(".exp").textContent = pokemon.base_experience;
+  const clone = $template.cloneNode(true);
+
+  $fragment.appendChild(clone);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
